32249e6b11685903484a9b72b932688ee70bc8c6
Anton Herzog <info@antonherzog.com>
2018-10-23 08:18:34 +0200

    Add comments.

--- Profile.ps1
+++ Profile.ps1
@@ -1,9 +1,10 @@
-
+# import all modules in this folder
 Get-ChildItem -Filter "*.psm1" -File "~\Github\PsDevScripts" | ForEach-Object {Import-Module $_.FullName}
 
-
+# activate modules you want always to be active
 Import-Module -Force posh-git, posh-docker
 
+# configure posh-git
 $global:GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 $global:GitPromptSettings.BeforeText = '['
 $global:GitPromptSettings.AfterText  = '] '
