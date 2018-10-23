89c70f906de39d9a5096e129014e79b9767e6f9f
Anton Herzog <info@antonherzog.com>
2018-10-23 08:22:51 +0200

    Add force to profile manipulating function.

--- PowershellConfig.psm1
+++ PowershellConfig.psm1
@@ -1,6 +1,6 @@
 function Edit-PowershellPrompt{
   if (!(Test-Path -Path $PROFILE)){
-    New-Item -Path $PROFILE -ItemType File 
+    New-Item -Path $PROFILE -Force -ItemType File 
   } 
   vim $PROFILE
 }
