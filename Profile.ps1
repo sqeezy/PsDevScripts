32d4c5f1e1d3dcd8c1e69eececb14756d56a2553
Anton Herzog <info@antonherzog.com>
2018-10-27 08:28:35 +0200

    Add alias to default profile

--- Profile.ps1
+++ Profile.ps1
@@ -9,4 +9,8 @@ $global:GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 $global:GitPromptSettings.BeforeText = '['
 $global:GitPromptSettings.AfterText  = '] '
 
+# add nice alias for git log formatting
+function GitLogBeauty {git log --pretty=format:"%h%x09%an%x09%ad%x09%s"}
+Set-Alias -Name glog -Value GitLogBeauty
+
 # additional functions that are machine specific
