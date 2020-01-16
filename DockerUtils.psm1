61dd5afed6319933673ec50e52d5922c704e0936
Anton Herzog <info@antonherzog.com>
2020-01-16 17:31:55 +0100

    Add docker stop before removing

--- DockerUtils.psm1
+++ DockerUtils.psm1
@@ -3,7 +3,20 @@ function Remove-AllUntagedDockerImages {
 }
 
 function Remove-AllDockerContainers {
-  docker ps -a -q | % { docker rm $_ }
+  docker ps -aq | ForEach-Object { docker stop -t 1 $_ } |  ForEach-Object { docker rm -vf $_ }
+}
+
+function Get-DockerRegistryContent {
+  [CmdletBinding()]
+  Param(
+  $Filter=".*",
+  #TODO: handle https & no basic auth as well..
+  $RegistryEndpoint = "sda-container-registry:5000"
+)
+
+  $allRepos = (Invoke-RestMethod -Uri "http://$RegistryEndpoint/v2/_catalog" -Method Get ).repositories 
+  $reposMatchingFilter = $allRepos -Match $Filter
+  $reposMatchingFilter | ForEach-Object {(Invoke-RestMethod -Uri "http://$RegistryEndpoint/v2/$_/tags/list" -Method Get )}
 }
 
 Export-ModuleMember -Function *
