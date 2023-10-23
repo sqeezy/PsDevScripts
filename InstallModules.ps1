7bf03dcb0a970f3e20daf1f63ac3043395d333d2
Anton Herzog <info@antonherzog.com>
2023-10-23 19:27:11 +0200

    Remove deprecated oh-my-posh module

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -1,3 +1,3 @@
-Install-Module -Scope CurrentUser -Force posh-git, posh-docker, oh-my-posh, Terminal-Icons, z
+Install-Module -Scope CurrentUser -Force posh-git, posh-docker, Terminal-Icons, z
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
