88fc0a643252ab09b8458d921248ca75e5d4081a
Anton Herzog <info@antonherzog.com>
2025-02-09 11:46:54 +0100

    Add installation of oh-my-posh and FiraCode

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -2,3 +2,8 @@ Install-Module -Scope CurrentUser -Force posh-git, posh-docker, Terminal-Icons, 
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install gh -s winget
+gh extension install github/gh-copilot
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+oh-my-posh font install FiraCode
+
